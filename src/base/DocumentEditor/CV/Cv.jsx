d04d9de5e5abb0120b8a21505404298a7857bd7e
Antony-AK <antonyak147@gmail.com>
2025-06-24 10:23:13 +0530

    COmmit

--- src/base/DocumentEditor/CV/Cv.jsx
+++ src/base/DocumentEditor/CV/Cv.jsx
@@ -34,11 +34,7 @@ const Cv = () => {
             image: { type: 'jpeg', quality: 0.98 },
             html2canvas: { scale: 2, useCORS: true },
             jsPDF: { unit: 'px', format: [794, 1123], orientation: 'portrait' }
-            margin:       0,
-            filename:     'ModernDeddy_CV.pdf',
-            image:        { type: 'jpeg', quality: 0.98 },
-            html2canvas:  { scale: 2 },
-            jsPDF:        { unit: 'in', format: 'a4', orientation: 'portrait' }
+            
         };
 
         html2pdf().set(opt).from(element).save();
